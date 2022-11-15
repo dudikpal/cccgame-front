f15269d9dc430b84d764d29ebf3de285d501228a
Dudik Pál <dudikpal@gmail.com>
2022-11-15 20:51:45 +0100

    refactor calculations

--- src/app/admin-page/input-field/input-field.component.ts
+++ src/app/admin-page/input-field/input-field.component.ts
@@ -1,17 +1,20 @@
 import {AfterViewInit, Component, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
-import {InitProps} from "../../admin-page/admin-page.component";
 import {environment} from "../../../environments/environment";
 import {EventService} from "../../event.service";
-import {main} from "@popperjs/core";
-import {AdminService} from "../../services/admin.service";
-import {coerceNumberProperty} from "@angular/cdk/coercion";
+import {AdminService} from "../../services/admin.service";;
 
+interface MultiplierValues {
+    optionalMultiplierValue: number,
+    baseMultiplierValue: number,
+}
 
 @Component({
     selector: 'app-input-field',
     templateUrl: './input-field.component.html',
     styleUrls: ['./input-field.component.css']
 })
+
+
 export class InputFieldComponent implements OnInit, AfterViewInit, OnChanges {
 
     @Input() initAllProps!: any;
@@ -20,17 +23,11 @@ export class InputFieldComponent implements OnInit, AfterViewInit, OnChanges {
 
     url = environment.endpointPrefix + '/api/cards';
 
-    multiplierFieldValue: any;
-
-
-
     constructor(private mainService: EventService,
                 private adminService: AdminService) {
     }
 
     ngOnInit(): void {
-        //console.log(this.initProps);
-
     }
 
     ngAfterViewInit(): void {
@@ -65,22 +62,6 @@ export class InputFieldComponent implements OnInit, AfterViewInit, OnChanges {
         return newCard;
     }
 
-
-    getPropValue(prop: String) {
-
-        console.log(this.selectedCard);
-        //console.log(this.selectedCard.card.value[`${prop}`].value);
-        return this.selectedCard[`${prop}`].value;
-        //return null;
-    }
-
-
-    updateCard() {
-
-        /*const updatedCard = this.inputFieldsToCard();
-        this.mainService.updateCard.emit(updatedCard);*/
-    }
-
     update() {
 
         let updatedCard = this.inputFieldsToCard();
@@ -90,7 +71,7 @@ export class InputFieldComponent implements OnInit, AfterViewInit, OnChanges {
     calculateTuningField(identifier: string) {
 
         const multiplierPropertyIdentifier = identifier.replace('tuning_', '');
-        console.log(identifier);
+
         switch (multiplierPropertyIdentifier) {
             case 'chassis':
                 this.calculateChassisTuning(identifier, multiplierPropertyIdentifier);
@@ -105,27 +86,31 @@ export class InputFieldComponent implements OnInit, AfterViewInit, OnChanges {
     }
 
     calculateEngineTuning(identifier: string, multiplierPropertyIdentifier: string) {
-        const optionalMultiplierValue = this.getOptionalMultiplierValue(identifier);
-        const baseMultiplierValue = this.getBaseMultiplierValue(multiplierPropertyIdentifier);
-        this.calculateCellValue('powerHP', optionalMultiplierValue, baseMultiplierValue);
-        this.calculateCellValue('acceleration', optionalMultiplierValue, baseMultiplierValue);
-        this.calculateCellValue('topSpeed', optionalMultiplierValue, baseMultiplierValue);
+        const multiplierValues = this.getMultiplierValues(identifier, multiplierPropertyIdentifier);
+        this.calculateCellValue('powerHP', multiplierValues);
+        this.calculateCellValue('acceleration', multiplierValues);
+        this.calculateCellValue('topSpeed', multiplierValues);
     }
 
     calculateCorneringTuning(identifier: string, multiplierPropertyIdentifier: string) {
-        const optionalMultiplierValue = this.getOptionalMultiplierValue(identifier);
-        const baseMultiplierValue = this.getBaseMultiplierValue(multiplierPropertyIdentifier);
-        this.calculateCellValue('width', optionalMultiplierValue, baseMultiplierValue);
-        this.calculateCellValue('height', optionalMultiplierValue, baseMultiplierValue);
-        this.calculateCellValue('groundClearance', optionalMultiplierValue, baseMultiplierValue);
+        const multiplierValues = this.getMultiplierValues(identifier, multiplierPropertyIdentifier);
+        this.calculateCellValue('width', multiplierValues);
+        this.calculateCellValue('height', multiplierValues);
+        this.calculateCellValue('groundClearance', multiplierValues);
         this.calculateCornering();
     }
 
     calculateChassisTuning(identifier: string, multiplierPropertyIdentifier: string) {
-        const optionalMultiplierValue = this.getOptionalMultiplierValue(identifier);
-        const baseMultiplierValue = this.getBaseMultiplierValue(multiplierPropertyIdentifier)
-        this.calculateCellValue('weight', optionalMultiplierValue, baseMultiplierValue);
+        const multiplierValues = this.getMultiplierValues(identifier, multiplierPropertyIdentifier);
+        this.calculateCellValue('weight', multiplierValues);
         this.calculateCornering();
+    }
+
+    getMultiplierValues(identifier: string, multiplierPropertyIdentifier: string) {
+        return {
+            optionalMultiplierValue: this.getOptionalMultiplierValue(identifier),
+            baseMultiplierValue: this.getBaseMultiplierValue(multiplierPropertyIdentifier)
+        };
     }
 
     calculateCornering() {
@@ -133,7 +118,7 @@ export class InputFieldComponent implements OnInit, AfterViewInit, OnChanges {
         corneringCell.value = 'calculated';
     }
 
-    calculateCellValue(identifier: string, optionalMultiplierValue: number, baseMultiplierValue: number) {
+    calculateCellValue(identifier: string, multiplierValues: MultiplierValues) {
         const baseValue = this.getBasicPropertyValue(identifier);
         const calculatedCell = this.getCalculatedCell(identifier);
 
@@ -143,12 +128,12 @@ export class InputFieldComponent implements OnInit, AfterViewInit, OnChanges {
             case 'acceleration':
             case 'groundClearance':
             case 'height':
-                calculatedCell.value = String(baseValue * (1 - (optionalMultiplierValue * baseMultiplierValue)));
+                calculatedCell.value = String(baseValue * (1 - (multiplierValues.optionalMultiplierValue * multiplierValues.baseMultiplierValue)));
                 break;
             case 'topSpeed':
             case 'width':
             case 'powerHP':
-                calculatedCell.value =  String(baseValue * (1 + (optionalMultiplierValue * baseMultiplierValue)));
+                calculatedCell.value =  String(baseValue * (1 + (multiplierValues.optionalMultiplierValue * multiplierValues.baseMultiplierValue)));
                 break;
         }
     }
@@ -167,61 +152,5 @@ export class InputFieldComponent implements OnInit, AfterViewInit, OnChanges {
 
     getBaseMultiplierValue(multiplierPropertyIdentifier: string) {
         return Number((this.mainService.tuningMultipliers as any)[`${multiplierPropertyIdentifier.toUpperCase()}`]);
-    }
-
-    calcFieldsInit() {
-// mikor fusson le, clickkor a list'ban, de ay hol van?
-        this.calculateTuningField('ENGINE');
-        /*console.log('calcfield funtion: ');
-        console.log(multiplierPropertyIdentifier);
-const baseMultiplier = (this.mainService.tuningMultipliers as any)['ENGINE'];
-
-        let optionalMultiplierCell = document.querySelector(`#input_${multiplierPropertyIdentifier}_playerCard`)as HTMLInputElement;
-        let optionalMultiplierValue = Number(optionalMultiplierCell.value);
-        const baseValue = this.selectedCard.card.value[multiplierPropertyIdentifier].value;
-        const multipliedCell = document.querySelector(`#input_${multiplierPropertyIdentifier}`)as HTMLInputElement
-        const multipliedValue = Number(multipliedCell.value);
-        multipliedCell.value = String(multipliedValue * optionalMultiplierValue * baseMultiplier);
-        console.log(multipliedCell);*/
-        /*
-        Tunings:
-            weight:
-                --weight
-                ++top speed
-                ++acc
-
-            engine:
-                ++power
-                ++top speed
-                ++acc
-
-            cornering:
-                ++width
-                --height
-         */
-        //const calculatedPropertyIdentifier = multiplierPropertyIdentifier.replace('tuning', '').toLowerCase();
-        //const calculatedPropertyName = calculatedPropertyIdentifier.toUpperCase();
-        //const calculatedPropertyField = document.querySelector(`#input_${calculatedPropertyIdentifier}`) as HTMLInputElement;
-        //const multiplier = this.mainService.tuningMultipliers[calculatedPropertyName];
-// @ts-ignore
-        //this.selectedCard.card.value[`${calculatedPropertyIdentifier}`].value = calculatedPropertyField.value * this.multiplierFieldValue * multiplier
-        // = 0
-        // :
-        // {identifier: 'tuningWeight', name: 'Tuning Weight', value: 1}
-        // 1
-        // :
-        // {identifier: 'tuningEngine', name: 'Tuning Engine', value: 1}
-        // 2
-        // :
-        // {identifier: 'tuningCornering', name: 'Tuning Cornering', value: 1}
-        //calculatedPropertyField.value = this.multiplierFieldValue * multiplier;
-        //console.log('card value before change: ' + this.selectedCard.card.value[propName].value);
-        //console.log(this.selectedCard);
-        /*console.log(calculatedPropertyIdentifier);
-        //console.log(propertyField.value);
-        console.log(Object.keys(multipliers));
-        console.log()*/
-        //console.log('card value before change: ' + this.selectedCard.card.value[propName].value);
-
     }
 }
