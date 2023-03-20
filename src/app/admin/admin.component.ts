668e43da1ee6eb2b6dc997c3752d46a8589aea69
Dudik Pál <dudikpal@gmail.com>
2023-03-21 00:37:33 +0100

    add loadBaseCardsFromFile, and dropBaseCardsTable

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+	selector: 'app-admin',
+	templateUrl: './admin.component.html',
+	styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
 
-  constructor() {
-  }
+	isCollapsed = false;
+	url = "http://localhost:8080/api/basecard"
+
+	constructor() {
+	}
+
+	loadBaseCardsFromFile() {
+		let input = document.createElement('input');
+		input.type = 'file';
+
+		input.onchange = e => {
+			let file = input.files![0];
+			let reader = new FileReader();
+			reader.readAsText(file, 'UTF-8');
+
+			reader.onload = readerEvent => {
+				let content = readerEvent.target!.result;
+
+				fetch(this.url + '/bulk', {
+					method: "POST",
+					body: content,
+					headers: {
+						"Content-Type": "application/json"
+					}
+				});
+			}
+		}
+		input.click();
+	}
+
+	dropBaseCardsTable() {
+		if (confirm('Will be delete all baseCards from collection!!\nAre you sure?')) {
+			fetch(this.url, {
+				method: "DELETE"
+			});
+		}
+	}
 }
