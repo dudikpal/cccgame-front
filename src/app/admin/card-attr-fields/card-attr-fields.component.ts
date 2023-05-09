730a911529bb8c8e68545ad7d688e9a05a7d8cc3
Dudik Pál <dudikpal@gmail.com>
2023-05-09 23:29:32 +0200

    add update baseCard button

--- src/app/admin/card-attr-fields/card-attr-fields.component.ts
+++ src/app/admin/card-attr-fields/card-attr-fields.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {IBaseCard} from "../../models/IBaseCard.";
+import {environment} from "../../../environments/environment";
 
 @Component({
 	selector: 'app-card-attr-fields',
@@ -12,5 +13,23 @@ export class CardAttrFieldsComponent {
 
 	getObjectEntries(baseCard: any) {
 		return Object.entries(baseCard);
+	}
+
+	async updateBaseCard() {
+		const attrFields = document.querySelectorAll('[data-card_attribute]');
+		for (const attrField of Array.from(attrFields)) {
+			const attributeName = attrField.getAttribute('data-card_attribute')!;
+			const value = (attrField as HTMLInputElement).value.trim();
+			(this.baseCard as any)[attributeName] = value;
+		}
+		const response = await fetch(environment.endpointPrefix + '/api/basecard',
+			{
+				method: "PUT",
+				body: JSON.stringify(this.baseCard),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			});
+		const data = await response.json();
 	}
 }
