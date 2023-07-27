7b68cf9e728b7f27399797ba575ca5b511932401
Dudik Pál <dudikpal@gmail.com>
2023-07-27 22:13:14 +0200

    drag&drop working properly

--- src/app/pages/choose-cards/choose-cards.component.ts
+++ src/app/pages/choose-cards/choose-cards.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from "@angular/cdk/drag-drop";
+import {CdkDragDrop, copyArrayItem, moveItemInArray, transferArrayItem} from "@angular/cdk/drag-drop";
 import {PlayerCard} from "../../models/PlayerCard";
 import {MatExpansionPanel} from "@angular/material/expansion";
 import {MainService} from "../../services/main.service";
@@ -43,21 +43,34 @@ export class ChooseCardsComponent implements OnInit{
 
     console.log('choosecard ondropban');
 
-    const cardId = event.previousContainer.data[event.previousIndex].id;
+    const cardId = event.item.data.id;
+    const cardName = event.previousContainer.data[event.previousIndex].baseCard.id;
     const card = document.querySelector(`#card_${CSS.escape(cardId)}`);
     card!.classList.add('disabled');
     //console.log(card!.classList.value);
-    this.mainService.onDrop(event);
-
+    //this.mainService.onDrop(event);
+    //console.log(event.previousContainer.data);
     //console.log(event.previousContainer.data[event.previousIndex].baseCard.imageUrl);
-    console.log(event.previousIndex);
-    console.log(cardId);
+    //console.log(event.previousIndex);
+    //console.log(cardId);
+    //console.log(cardName);
     //console.log(this.selectedCard1);
     //this.selectedCard1 = [event.previousContainer.data[event.previousIndex]];
-    this.selectedCard1 = this.selectedCards.filter(
+    /*this.selectedCard1 = this.selectedCards.filter(
         (card: { id: any; }) => card.id === event.item.data.id
-    )
-    console.log(event.item.data.id);
+    );*/
+    const cardIndex = this.selectedCards.findIndex((card: { id: any; }) => card.id === cardId);
+    //console.log(cardIndex);
+    copyArrayItem(this.selectedCards,
+        event.container.data,
+        cardIndex,
+        0);
+    console.log(this.selectedCard1);
+    console.log(this.selectedCard2);
+    console.log(this.selectedCard3);
+    console.log(this.selectedCard4);
+    console.log(this.selectedCard5);
+    //console.log(event.item.data.id);
 
   }
 
@@ -72,5 +85,13 @@ export class ChooseCardsComponent implements OnInit{
 
   toggleDisable() {
     this.mainService.toggleDisable();
+  }
+
+  rechooseCards() {
+    this.selectedCard1 = [this.mainService.playerCardSkeleton];
+    this.selectedCard2 = [this.mainService.playerCardSkeleton];
+    this.selectedCard3 = [this.mainService.playerCardSkeleton];
+    this.selectedCard4 = [this.mainService.playerCardSkeleton];
+    this.selectedCard5 = [this.mainService.playerCardSkeleton];
   }
 }
