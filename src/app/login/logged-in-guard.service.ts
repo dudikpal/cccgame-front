f4f3c07097daa871e3028a0ec17d9c7466eb84de
Dudik Pál <dudikpal@gmail.com>
2023-10-28 08:04:18 +0200

    add security

---
+++ src/app/login/logged-in-guard.service.ts
@@ -0,0 +1,98 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
+import {MainService} from "../services/main.service";
+//import {NGXLogger} from "ngx-logger";
+
+@Injectable()
+export class LoggedInGuardService implements CanActivate {
+
+    public token!: string | null;
+    public userId = '';
+    public garageId = "empty";
+    endpointPrefix = environment.endpointPrefix;
+    public cards: any[] = [];
+    public isLoading = false;
+    public playerCardSkeleton: any;
+
+
+    constructor(private _router: Router,
+                private mainService: MainService,
+    //            private logger: NGXLogger
+    ) {
+    }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+
+        if (!this.mainService.userIsLoggedIn) {
+            //this.mainService.playerCardSkeleton = this.playerCardSkeleton;
+            this._router.navigate(['/login']);
+            return false;
+        }
+
+        return true;
+    }
+
+
+
+    async sendLoginDataToBack(username: string, password: string) {
+
+        const reqBody = JSON.stringify({"username": username, "password": password});
+        const response = await fetch(this.endpointPrefix + "/api/auth/signin", {
+            method: 'POST',
+            body: reqBody,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        if (response.status < 400) {
+
+            const jsonData = await response.json();
+            this.token = jsonData['jwt'].match(/(?<=ccgamer=).*?(?=;)/g).toString();
+            //this.logger.info("Login successful");
+            //this.logger.info("Response content: ", jsonData);
+            sessionStorage.setItem('AuthToken', JSON.stringify(this.token));
+            this.mainService.userIsLoggedIn = true;
+            await this.mainService.getPlayerCards();
+            await this.mainService.getPlayerCardSkeleton();
+            this.userId = jsonData['id'];
+            this.garageId = jsonData['garageId'];
+
+            this._router.navigate(['/home']);
+            return true;
+
+        } else {
+            return false;
+        }
+
+    }
+
+
+    async sendRegisterDataToBack(username: string, email: string, roles: [], password: string) {
+
+        const reqBody = JSON.stringify({"username": username, "email": email, "password": password});
+        const response = await fetch(this.endpointPrefix + "/api/auth/signup", {
+            method: 'POST',
+            body: reqBody,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        return response.text();
+    }
+
+
+    parseJwt(token: string) {
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+        console.log(jsonPayload);
+
+        return JSON.parse(jsonPayload);
+    };
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,49 @@
+import {Component, Injectable, Input, OnDestroy, OnInit} from '@angular/core';
+import {LoggedInGuardService} from "./logged-in-guard.service";
+import {AppComponent} from "../app.component";
+import {Router} from "@angular/router";
+import {MainService} from "../services/main.service";
+
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit, OnDestroy {
+
+    public username = '';
+    public password = '';
+    public loginValid = true;
+    //public isLoading = this.mainService.isLoading;
+
+    constructor(
+        private authService: LoggedInGuardService,
+        private appComponent: AppComponent,
+        private router: Router,
+        public mainService: MainService,
+    ) {
+    }
+
+    public ngOnInit(): void {
+
+    }
+
+    public ngOnDestroy(): void {
+    }
+
+    public async onSubmit() {
+        this.mainService.isLoading = true;
+        this.mainService.userIsLoggedIn = await this.authService.sendLoginDataToBack(this.username, this.password);
+        this.mainService.isLoading = false;
+    }
+
+    toRegistration() {
+        this.router.navigate(['/register']);
+    }
+
+    public showSpinner(): void {
+
+
+    }
+}
